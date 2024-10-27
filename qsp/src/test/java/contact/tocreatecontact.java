c8e9e1eca134aaf2a73e58864a08f33f77134eb7
Lenovo <Lenovo@LAPTOP-LU0D8EQR>
2024-10-27 19:10:39 +0530

    first push

---
+++ qsp/src/test/java/contact/tocreatecontact.java
@@ -0,0 +1,9 @@
+package contact;
+
+public class tocreatecontact {
+
+	public static void main(String[] args) {
+System.out.println("contact created ");
+	}
+
+}
---
+++ qsp/src/test/java/org/tocreateorg.java
@@ -0,0 +1,9 @@
+package org;
+
+public class tocreateorg {
+
+	public static void main(String[] args) {
+System.out.println("org created");
+	}
+
+}
